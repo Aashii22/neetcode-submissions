e508808227f74de4926f55153ed7b9ac976c25b4
Aashii22 <88074732+Aashii22@users.noreply.github.com>
2026-07-26 23:09:46 +0530

    Add: duplicate-integer - submission-0

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        map<int, int> mp;
+        for(int x: nums){
+            if(mp[x])
+            return true;
+
+            mp[x]++;
+        }
+
+        return false;
+    }
+};
